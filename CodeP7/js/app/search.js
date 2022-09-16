34afe357cce7af225bb3ea7c8d66f1c88ca4faff
BFlorian59 <florianbroeks59@gmail.com>
2022-09-16 16:20:09 +0200

    search native

--- CodeP7/js/app/search.js
+++ CodeP7/js/app/search.js
@@ -100,29 +100,29 @@ class  Search{
                     let recette = [];
                     //console.log(ingre);
                     recette = tabIngrediants.find(x => x.ingredient == ingre.ingredient);
-                    console.log(tabIngrediants)
+                    //console.log(tabIngrediants)
                     // for (let index = 0; index < tabIngrediants.length; index++) {
 
                     //     const element = tabIngrediants[index];
                     //     if (element.ingredient == ingre.ingredient) {                              
-                    //         return recette;
+                    //          recette = element;
                     //     }
-                        
+                    //     return recette;
                     // }
 
                     if(recette != undefined){
                     //TODO ici vérifier si l'id de la recette n'est pas déjà ajouté.
                         
-                        if (!recette.id == false) {
-                            recette.id.push(recipe.id);
-                            console.log(recette.id);
-                        }
-                        
-                        // if(!recette.id.includes(recipe.id)){ 
+                        // if (!recette.id == false) {
                         //     recette.id.push(recipe.id);
+                        //     console.log(recette.id);
                         // }
-                        // tabIngrediants.slice(tabIngrediants.indexOf(recette),1);
-                        console.log(tabIngrediants)
+                        
+                        if(!recette.id.includes(recipe.id)){ 
+                            recette.id.push(recipe.id);
+                        }
+                        tabIngrediants.slice(tabIngrediants.indexOf(recette),1);
+                        //console.log(tabIngrediants)
                         // for (let index = 0; index < tabIngrediants.length; index++) {
                             
                         //     const element = tabIngrediants[index];
