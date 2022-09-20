4a4fe0d47ddcf7659bd4c265d0cd9119f99fb6fc
BFlorian59 <florianbroeks59@gmail.com>
2022-09-20 14:56:00 +0200

    search native

--- CodeP7/js/app/search.js
+++ CodeP7/js/app/search.js
@@ -33,19 +33,21 @@ class  Search{
                 const input_search = input.value;
                 //compléter la recherche (sur les ingrédients ... description )
                 let tabingreselcted = [];
-                
+                console.log('halo')
                 let resultIngre = getLstRecipes(lstrecipe)
                 console.log(resultIngre)
-                //var tabIngredients_filtre =  resultIngre.filter(tab => tab.ingredient.toLocaleLowerCase().includes(input_search.toLocaleLowerCase()));
-                var tabIngredients_filtre = [];
-                for(let i = 0; i < resultIngre.length; i++ ){
-                    for(let j = 0; j < resultIngre[i].ingredient.length; j++ ){
-                        if(resultIngre[i].ingredient[j].toLocaleLowerCase() == input_search.toLocaleLowerCase()){
-                            tabIngredients_filtre.push(resultIngre[i]);
-                           // TODO à tester 'break;'
-                        }
-                    }
-                }
+                var tabIngredients_filtre =  resultIngre.filter(tab => tab.ingredient.toLocaleLowerCase().includes(input_search.toLocaleLowerCase()));
+                // var tabIngredients_filtre = [];
+                // for(let i = 0; i < resultIngre.length; i++ ){
+                //     for(let j = 0; j < resultIngre[i].ingredient.length; j++ ){
+                //         console.log(resultIngre[i].ingredient)
+                //         console.log(input_search)
+                //         if(resultIngre[i].ingredient[j].toLocaleLowerCase() == input_search.toLocaleLowerCase()){
+                //             tabIngredients_filtre.push(resultIngre[i]);
+                //             console.log(tabIngredients_filtre)
+                //         }
+                //     }
+                // }
                 console.log("my filter")
                 console.log(tabIngredients_filtre)
 
@@ -70,11 +72,11 @@ class  Search{
                 }
                 
             
-                //var result_searchs = lstrecipe.filter(search => search.name.toLocaleLowerCase().includes(input_search)||search.description.toLocaleLowerCase().includes(input_search));
+                var result_searchs = lstrecipe.filter(search => search.name.toLocaleLowerCase().includes(input_search)||search.description.toLocaleLowerCase().includes(input_search));
                 
 
                 
-                 var result_searchs = [];
+                // var result_searchs = [];
                 // for(let i = 0; i < lstrecipe.length; i++ ){
                 //     for(let j = 0; j < lstrecipe[i].name.length; j++ ){
                 //         console.log(input_search)
