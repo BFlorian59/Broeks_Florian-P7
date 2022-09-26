249bb1416accf6083eee0c03dd2854362b7e5226
BFlorian59 <florianbroeks59@gmail.com>
2022-09-26 20:45:14 +0200

    search  + tag

--- CodeP7/js/app/search.js
+++ CodeP7/js/app/search.js
@@ -11,6 +11,108 @@
         this.tabsearchsset =[];
     }
    
+    search(){
+        const $recette = document.querySelector(".recette");
+        var lstrecipe = this.recipe;
+        var tabsearchsset = []
+       
+        var buttonsearch = document.querySelector(".Recherche-Icone");
+        const input = document.querySelector('.Recherche-Input');
+        //condition tag selectionné
+        if(this.tabtag.length > 0){
+            lstrecipe =  this.addTagSearch();
+            console.log(lstrecipe)
+        }
+
+        if (this.tabtag.length < 1) {
+            lstrecipe = this.recipe;
+        }
+
+        // commencer la recherche si tu as plus de 3 caracteres
+        if (input.value.length > 2 ) {
+
+            const input_search = input.value;
+            //compléter la recherche (sur les ingrédients ... description )
+            let tabingreselcted = [];
+            
+            let resultIngre = getLstRecipes(lstrecipe);
+             // filtrer les ingredient qui correspond au mot clé
+            var tabIngredients_filtre =  resultIngre.filter(tab => tab.ingredient.toLocaleLowerCase().includes(input_search.toLocaleLowerCase()));
+            // console.log("result après add Carotte tab ingredient ");
+            // console.log(tabIngredients_filtre);
+            let lstRecipesSelected = [];
+            // tabingreselcted => liste de recttes selectionnés sur la liste global par rapport aux ingrédiants
+             // mette dans un tableau les recettes qui correspond au ingredient qui on était filtré 
+            tabIngredients_filtre.forEach(element => {
+                element.id.forEach(id => {
+                    lstRecipesSelected.push(this.recipe[id-1]);         
+                })           
+            });
+
+            // filtrer les recettes qui correspond au mot clé présent dans le nom ou dans la description
+            var result_searchs = lstrecipe.filter(search => search.name.toLocaleLowerCase().includes(input_search)||search.description.toLocaleLowerCase().includes(input_search));
+
+            // fusionner le tableau des recette filtré avec les ingrédients avec le tableau des recette filtré avec le nom et la description
+            const tabsearchs = result_searchs.concat(lstRecipesSelected);
+            this.tabsearchsset = [...new Set(tabsearchs)];
+            tabsearchsset = this.tabsearchsset
+
+            const search = new Resultsearch(this.recipe, this.tabsearchsset);
+            search.displaysearch();
+           
+            if (this.tabsearchsset.length == 0) {
+                var error ='';
+                error = `
+                <div id= "error">
+                    <p>Aucune recette ne correspond à votre critère</p>
+                </div>
+                    `
+
+                $recette.innerHTML = error;
+            }
+            
+            return tabsearchsset
+        }else if (input.value.length < 3){
+
+            $recette.innerHTML ='';
+           
+            this.recipe.forEach((recipe) => {                    
+                const pCard = new Recette_card(recipe);
+                const pCardElement = pCard.createrecette();
+                $recette.appendChild(pCardElement); 
+            
+            });
+        }
+        function getLstRecipes( lstRecipes){
+            let tabIngredients= [];
+            lstRecipes.forEach((recipe) => {
+               
+    
+                let lstingredient = null;
+                recipe.ingredients.forEach(ingre => {   
+                    lstingredient = null;
+                    lstingredient = tabIngredients.find(x => x.ingredient == ingre.ingredient);
+                    if(lstingredient != undefined){
+                        //ici vérifier si l'id de la recette n'est pas déjà ajouté.
+                        if(!lstingredient.id.includes(recipe.id)){
+                            lstingredient.id.push(recipe.id);
+                        }
+    
+                        tabIngredients.slice(tabIngredients.indexOf(lstingredient),1);
+                    }else{              
+                        lstingredient = {
+                            ingredient: ingre.ingredient,
+                            id: [recipe.id]
+                        };
+                    }
+                    tabIngredients.push(lstingredient);
+                    //console.log(tabIngrediants)
+                });
+            });
+           
+            return tabIngredients;
+        }
+    }
 
     // recherche mot clé 
     globalSearch(){
@@ -185,10 +287,11 @@
     addEventtag (){
         document.querySelectorAll('.items').forEach(ingredient => {
             ingredient.addEventListener('click', () => {
+                console.log("ptn d'ingredients")
                 this.tagingre.push(ingredient.innerHTML);
                 this.removeTagSearch();
                 this.addTagSearch();
-                this.globalSearch();
+                this.search();
             });
         });
 
@@ -199,7 +302,7 @@
                 this.tagapp.push(appliance.innerHTML);
                 this.addTagSearch();
                 this.removeTagSearch();
-                this.globalSearch();
+                this.search();
             });
         });
 
@@ -208,7 +311,7 @@
                 this.tagust.push(ustensils.innerHTML);
                 this.addTagSearch();
                 this.removeTagSearch();
-                this.globalSearch();
+                this.search();
             });
         });
 
@@ -216,10 +319,11 @@
         Ingredientinput.addEventListener('keyup', () =>{
             document.querySelectorAll('.items').forEach(ingredient => {
                 ingredient.addEventListener('click', () => {
+                    console.log("lala")
                     this.tagingre.push(ingredient.innerHTML);
                     this.addTagSearch();
                     this.removeTagSearch();
-                    this.globalSearch();
+                    this.search();
                 });
             });
     
@@ -229,10 +333,11 @@
         Appareilinput.addEventListener('keyup', () =>{
             document.querySelectorAll('.items3').forEach(appliance => {
                 appliance.addEventListener('click', () => {
+                    console.log("lala")
                     this.tagapp.push(appliance.innerHTML);
                     this.addTagSearch();
                     this.removeTagSearch();
-                    this.globalSearch();
+                    this.search();
                 });
             });
     
@@ -242,10 +347,11 @@
         Ustensilsinput.addEventListener('keyup', () =>{
             document.querySelectorAll('.items2').forEach(ustensils => {
                 ustensils.addEventListener('click', () => {
+                    console.log("lala")
                     this.tagust.push(ustensils.innerHTML);
                     this.addTagSearch();
                     this.removeTagSearch();
-                    this.globalSearch();
+                    this.search();
                 });
             });
     
